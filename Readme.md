0b5b3a866a0f0fcc2471c7f1925958313822bf07
iwyg <mail@thomas-appel.com>
2012-02-08 20:40:56 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,41 @@
+# Requirejs for Symphony-CMS
+
+### Abstract 
+This simply adds (Requirejs)[1] to your Symphony CMS backend. 
+
+### Installation
+in your smyphony root directory: `git submodule add git://github.com/iwyg/sym.requirejs extensions/requirejs --recursive`
+
+Go to `System->Extensions`, select requirejs and choose `enable/install`
+
+### Usage
+
+for example, you need to use the latest jQuery library instead of the Symphony default version: 
+
+		require({
+			baseUrl: url + '/extensions/myextension/assets/js',
+			paths: {
+				'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min'
+			}
+		}, ['main']);
+
+In your main.js you could do sommething like this:
+
+		(function (jOuery, require) {
+			// cache default jQuery for later reference
+			// because loading jquery will overwite the jQuery global
+			var $144 = jQuery;
+
+			require(['jquery'], function ($171) {
+				$171.noConflict(); // remove the $ global
+				jQuery = $144; re-reference the orignal jQuery instance;
+				
+				console.log(jQuery.fn.jquery); // "1.4.4"
+				console.log($144.fn.jquery);   // "1.4.4"
+				console.log($171.fn.jquery);   // "1.7.1"
+			});
+		}(this.jQuery, this.require));
+
+
+
+[1] http://requirejs.org/
---
+++ extension.driver.php
@@ -0,0 +1,61 @@
+<?php 
+	
+Class extension_requirejs extends Extension 
+{
+
+	public function about() 
+	{
+		return array(
+			'name' => 'requirejs',
+			'type'	=> 'script',
+			'version' => '1',
+			'release-date' => '2012-02-02',
+			'author' => array(
+				'name' => 'Thomas Appel',
+				'email' => 'mail@thomas-appel.com',
+				'website' => 'http://thomas-appel.com'
+			),
+			'description' => 'adds requirejs amd library to the backend',
+			'compatibility' => array(
+				'2.2' => true
+			)
+		);
+	}
+
+	public function __construct(Array $args) 
+	{
+		parent::__construct($args);
+
+		$reqirepath = EXTENSION . '/requirejs/assets/requirejs/require.js';
+
+		if(!is_file($path)) {
+			//throw new SymphonyErrorPage(__('Please make sure to include submodules as well. Try <code>git submodule update --init</code> from within the extensions/requirejs path'));
+		}
+	}
+
+	public function getSubscribedDelegates()
+	{
+		return array(
+
+			// Subsection Manager
+			array(
+				'page' => '/backend/',
+				'delegate' => 'AdminPagePreGenerate',
+				'callback' => '__appendAssets'
+			),
+		);
+	}
+
+	public function __appendAssets($context) 
+	{
+		$callback = Symphony::Engine()->getPageCallback();
+
+
+		// Append Script for publish area
+		if($callback['driver'] == 'publish') {
+			Administration::instance()->Page->addScriptToHead(URL . '/extensions/requirejs/assets/require.js', 111, false);
+		}
+	}
+}
+?>
+

f6b2e65f5814b260b406c59983a679d213f834d1
alexbirukov <lord_maloy@mail.ru>
2012-12-11 00:26:31 +0600

    Update Russian language
    Update Russian language file

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -7,42 +7,15 @@
 			'email' => 'info@alexbirukov.ru',
 			'website' => 'http://alexbirukov.ru'
 		),
-		'release-date' => '2012-06-18'
+		'release-date' => '2012-12-11'
 	);
 
 	/**
-	 * CKEditor
+	 * Requirejs
 	 */
 	$dictionary = array(
 
-		'CKEditor File Browser' => 
-		'Браузер файлов CKEditor',
-
-		'Permitted sections for the file browser:' => 
-		'Разделы доступные для браузера файлов:',
-
-		'Link templates:' => 
-		'Шаблоны ссылок:',
-
-		'Link template' => 
-		'Шаблон ссылки',
-
-		'Use {$id} for the entry ID, and {$fieldname} for field-placeholders. If the field has a handle, this is automatically used.' => 
-		'Используйте переменную {$id} для вывода ID записи и {$fieldname} для подстановки значения поля. Если полю назначен обработчик то он будет применён автоматически.',
-
-		'Section to get the entries from' => 
-		'Разделы для выборки записей',
-
-		'Field to display as name' => 
-		'Поле для отображения в качестве имени',
-
-		'Section' => 
-		'Раздел',
-
-		'There are no sections available. Please select which sections are permitted to use the CKEditor file upload feature in the Symphony System Preferences.' => 
-		'Не указаны доступные разделы. Пожалуйста, укажите в настройках системы разделы, которые доступны для загрузки файлов через CKEditor.',
-
-		'create new' => 
-		'добавить',
+		'Please make sure to include submodules as well. Try <code>git submodule update --init</code> from within the extensions/requirejs path' => 
+		'Пожалуйста, проверьте наличие всех необходимых модулей. Попробуйте выполнить <code>git submodule update --init</code> в каталоге extensions/requirejs',
 
 	);
